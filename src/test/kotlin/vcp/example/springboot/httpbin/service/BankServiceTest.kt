ca0c369c85ee24b2e10378063a3b0bc1ff4b5eeb
roj <viroj.chaipredanan@krungsri.com>
2022-09-22 19:15:41 +0700

    [test 3] refactor test mockk with relaxed = true

--- src/test/kotlin/vcp/example/springboot/httpbin/service/BankServiceTest.kt
+++ src/test/kotlin/vcp/example/springboot/httpbin/service/BankServiceTest.kt
@@ -1,6 +1,5 @@
 package vcp.example.springboot.httpbin.service
 
-import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Test
@@ -8,16 +7,16 @@ import vcp.example.springboot.httpbin.datasource.BankDataSource
 
 internal class BankServiceTest {
 
-    private val dataSource: BankDataSource = mockk()
+    private val dataSource: BankDataSource = mockk(relaxed = true)
     private val bankService = BankService(dataSource)
 
     @Test
     fun `should call its data source to retrieve banks`() {
         //given
-        every { dataSource.getBanks() } returns emptyList()
-        
+
+
         //when
-        val banks = bankService.getBanks()
+        bankService.getBanks()
         
         //then
         verify(exactly = 1) { dataSource.getBanks() }
