6a61d681325f03686785a723df09ea4dcac8c361
roj <viroj.chaipredanan@krungsri.com>
2022-09-22 19:34:22 +0700

    [test 4] refactor code to make BankControllerTest - green

--- src/main/kotlin/vcp/example/springboot/httpbin/controller/BankController.kt
+++ src/main/kotlin/vcp/example/springboot/httpbin/controller/BankController.kt
@@ -1,7 +1,14 @@
 package vcp.example.springboot.httpbin.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import vcp.example.springboot.httpbin.model.Bank
 
 @RestController
+@RequestMapping("/api/banks")
 class BankController {
+
+    @GetMapping
+    fun getBanks() : Collection<Bank> = emptyList()
 }
