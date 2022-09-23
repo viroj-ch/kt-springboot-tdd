da8de714cfb5490386c1a2301f3934410c41506e
roj <viroj.chaipredanan@krungsri.com>
2022-09-23 10:28:38 +0700

    refactor BankControllerTest using nested class
    don't forget to change intellij setting

--- src/test/kotlin/vcp/example/springboot/httpbin/controller/BankControllerTest.kt
+++ src/test/kotlin/vcp/example/springboot/httpbin/controller/BankControllerTest.kt
@@ -1,6 +1,10 @@
 package vcp.example.springboot.httpbin.controller
 
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -16,38 +20,44 @@ internal class BankControllerTest {
     @Autowired
     lateinit var mockMvc: MockMvc
     
-    @Test
-    fun `should return all banks`() {
-        //given
-        
-        
-        //when
-        //then
-        mockMvc.get("/api/banks")
-                .andDo { print() }
-                .andExpect {
-                    status { isOk() }
-                    content { contentType(MediaType.APPLICATION_JSON) }
-                    jsonPath("$[0].accountNumber") { value("1234")}
-                }
-        
+    @Nested
+    @DisplayName("GET /api/banks")
+    @TestInstance(Lifecycle.PER_CLASS)
+    inner class GetBanks {
+        @Test
+        fun `should return all banks`() {
+            //when
+            //then
+            mockMvc.get("/api/banks")
+                    .andDo { print() }
+                    .andExpect {
+                        status { isOk() }
+                        content { contentType(MediaType.APPLICATION_JSON) }
+                        jsonPath("$[0].accountNumber") { value("1234")}
+                    }
+        }
     }
-    
-    @Test
-    fun `should return the bank with given account number`() {
-        //given
-        val accountNumber = "1234"
-        
-        //when
-        //then
-        mockMvc.get("/api/banks/$accountNumber")
-                .andDo { print() }
-                .andExpect {
-                    status { isOk() }
-                    content { contentType(MediaType.APPLICATION_JSON) }
-                    jsonPath("$.accountNumber") { value("1234")}
-                    jsonPath("$.trust") { value(0.0)}
-                    jsonPath("$.transactionFee") { value(1)}
-                }
+
+    @Nested
+    @DisplayName("GET /api/banks/{accountNumber}")
+    @TestInstance(Lifecycle.PER_CLASS)
+    inner class GetBank {
+        @Test
+        fun `should return the bank with given account number`() {
+            //given
+            val accountNumber = "1234"
+
+            //when
+            //then
+            mockMvc.get("/api/banks/$accountNumber")
+                    .andDo { print() }
+                    .andExpect {
+                        status { isOk() }
+                        content { contentType(MediaType.APPLICATION_JSON) }
+                        jsonPath("$.accountNumber") { value("1234")}
+                        jsonPath("$.trust") { value(0.0)}
+                        jsonPath("$.transactionFee") { value(1)}
+                    }
+        }
     }
 }
